88ff8d6067e4082eb33a34f4ca71f6a927fd88c0
jiaiqi <2468260248@qq.com>
2022-09-13 22:56:18 +0800

    🎉 初始化项目

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import router from "./router";
+import store from "./store";
+// import "https://at.alicdn.com/t/c/font_3646349_28wfloukbm3.js"; //引入iconfont
+// import "@/assets/iconfont/iconfont.js"; //引入iconfont
+import "tailwindcss/tailwind.css"; // 引入tailwind
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
-
+Vue.use(ElementUI);
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
